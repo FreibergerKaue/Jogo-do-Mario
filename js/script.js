f9afba01775d1eb961b473e4925517832e10698d
FreibergerKaue <kauealunofreiberger@gmail.com>
2023-03-01 17:38:39 -0300

    @freiberger.kaue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const score = document.querySelector('.score');
+const gameOver = document.querySelector('.gameOver')
+const jogarnovamente = document.querySelector('.jogarnovamente')
+const die = document.querySelector('.die')
 
 let count = 0;
 
@@ -17,7 +20,7 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 120) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
@@ -27,54 +30,24 @@ const loop = setInterval(() => {
         mario.src = './images/game-over.png'
         mario.style.width = '95px'
         mario.style.marginLeft = '50px'
-        
+
+        jogarnovamente.style.color = '#000'
+
+        alert(`Você perdeu...Sua pontuação: ${count}`);
     count = 0;
 
         clearInterval(loop);
     }
     
     count++;
-    score.innerHTML = `SCORE: ${count}`;
+    score.innerHTML =  `SCORE: ${count}`;
     
 }, 10);
 
 document.addEventListener('keydown', jump);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-setInterval(() => {
-    let dinoBottom = parseInt(
-      window.getComputedStyle(dino).getPropertyValue("bottom")
-    );
-    let cactoLeft = parseInt(
-      window.getComputedStyle(cacto).getPropertyValue("left")
-    );
-  
-    if (cactoLeft > 40 && cactoLeft < 270 && dinoBottom <= 50 && !alreadyJump) {
-      alert(`Game Over! Seu score foi: ${count}`);
-      count = 0;
-    }
-  
-    count++;
-    score.innerHTML = `SCORE: ${count}`;
-  }, 10);
-  
+document.addEventListener('keydown', function(){
+    let audio1 = document.querySelector('audio', '.jumpSound')
+    audio1.play()
+});
